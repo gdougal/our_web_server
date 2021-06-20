11ed1dc707884037e120424073ede43bf0778958
lmallado <hoobchik@gmail.com>
2021-06-20 14:47:04 +0300

    response fixed

--- srcs/builders/ResponseBuilder.cpp
+++ srcs/builders/ResponseBuilder.cpp
@@ -5,10 +5,23 @@
 #include "ResponseBuilder.hpp"
 #include <http_stuff.hpp>
 
+
+ResponseBuilder::ResponseBuilder(server_config &serverConfig,
+                                 map<string, string> headers,
+                                 pair<string, string> path)
+    : serverConfig(serverConfig), headers(headers), path(path) {
+
+}
+
+bool ResponseBuilder::check_request_valid() {
+
+  return true;
+}
+
 string ResponseBuilder::build_response() {
   if (!check_request_valid())
     return build_error();
-  std::ifstream page("/goinfre/lmallado/our_web_server" + path.second);
+  std::ifstream page("/Users/lmallado/Desktop/our_web_server" + path.second);
   std::string body;
   std::string tmp;
   while (std::getline(page, tmp)) {
@@ -25,19 +38,11 @@ string ResponseBuilder::build_error() {
   return std::string();
 }
 
-bool ResponseBuilder::check_request_valid() {
-
-  return true;
-}
-
-ResponseBuilder::ResponseBuilder(server_config &serverConfig,
-                                 map<string, string> headers,
-                                 pair<string, string> path)
-    : serverConfig(serverConfig), headers(headers), path(path) {
-
-}
 string ResponseBuilder::build_headers() {
 
 
   return std::string();
 }
+
+
+
