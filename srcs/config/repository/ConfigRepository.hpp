8ed0359a878bf4e488cb10c6e4b70ad7f2e32827
lmallado <hoobchik@gmail.com>
2021-06-20 14:34:32 +0300

    response builder build started

--- srcs/config/repository/ConfigRepository.hpp
+++ srcs/config/repository/ConfigRepository.hpp
@@ -7,6 +7,7 @@
 
 #include "RouteEntity.hpp"
 #include <string>
+#include "list"
 
 using namespace std;
 
@@ -14,14 +15,16 @@ struct server_config {
   string host = "127.0.0.1";
   string port = "8080";
   string server_name = "default_name";
+  size_t client_body_size;
   string error_page_path = "defaut_path";
-  route *routes;
+  std::list<route> routes;
 
   server_config(const string &host, const string &port,
-                const string &serverName, const string &errorPagePath,
-                route *routes)
+                const string &serverName, size_t client_body_size, const string
+                                              &errorPagePath,
+                std::list<route> routes)
       : host(host), port(port), server_name(serverName),
-        error_page_path(errorPagePath), routes(routes) {}
+        error_page_path(errorPagePath), routes(routes), client_body_size(client_body_size) {}
 };
 
 #endif // WEB_SERVER_CONFIGREPOSITORY_HPP
