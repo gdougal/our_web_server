c67f14066c189765c2e6781ed4b887ce6c2a4bb5
gdougal <gdougal@student.21-school.ru>
2021-07-04 19:10:31 +0300

    post

--- srcs/builders/methods/Post.hpp
+++ srcs/builders/methods/Post.hpp
@@ -4,40 +4,47 @@
 
 #ifndef WEB_SERVER_POST_HPP
 #define WEB_SERVER_POST_HPP
+
 #include "ResponseBuilder.hpp"
 #include "fstream"
 
 class Post {
 private:
 
-	Post() = delete;
-	Post(const Post &ref) = delete;
-	Post &operator=(const Post &ref) = delete;
+  Post() = delete;
+
+  Post(const Post &ref) = delete;
+
+  Post &operator=(const Post &ref) = delete;
+
 public:
-	static std::string	put(const std::string& filename, const t_request_data& data) {
-		std::ofstream	outfile;
-		outfile.open( filename.c_str() );
-		std::string content_type(ResponseUtils::get_content_type(filename));
-		bool he = (data.header.find("CONNECTION-TYPE")->second == KEEP_ALIVE_STR);
+  static std::string  put(const std::string &filename, const t_request_data &data) {
+    std::ofstream outfile;
+    outfile.open(filename.c_str());
+    const std::string content_type(ResponseUtils::get_content_type(filename));
+    const bool b_connection = (data.header.find("CONNECTION")->second ==
+                               KEEP_ALIVE_STR);
 
-		if (outfile.is_open()) {
-			outfile.write(data.body.c_str(), data.body.size());
-			return HeadersBuilder::build(ER204,
-																	 static_cast<connection>(he),
-																	 content_type,
-																	 data.body.size() );
-		}
-		else {
-			outfile.open(filename.c_str());
-			outfile.write(data.body.c_str(), data.body.size());
-			return HeadersBuilder::build(R201,
-                            static_cast<connection>(he),
-                            content_type,
-                            data.body.size() );
-		}
-	};
+    if (outfile.is_open()) {
+      outfile.write(data.body.c_str(), data.body.size());
+      return HeadersBuilder::build(ER204,
+                                   static_cast<connection>(b_connection),
+                                   content_type,
+                                   data.body.size());
+    }
+    else {
+      outfile.open(filename.c_str());
+      outfile.write(data.body.c_str(), data.body.size());
+      return HeadersBuilder::build(R201,
+                                   static_cast<connection>(b_connection),
+                                   content_type,
+                                   data.body.size());
+    }
+  };
 
-	virtual ~Post() {};
+  static std::string  post(const std::string &filename, const t_request_data &data) {};
+
+  virtual ~Post() {};
 };
 
 
